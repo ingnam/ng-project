8746ff35f17f6e60efb59cc23624ba839421fb8f
Madan Ingnam <madingnm@gmail.com>
2015-06-17 18:30:27 +0545

    custom directive created

--- js/controllers/MainController.js
+++ js/controllers/MainController.js
@@ -13,13 +13,74 @@ app.controller('MainController', ['$scope', function($scope) {
 			name: 'The song of ice and fire',
 			price: 80,
 			pubdate: new Date('2009', '03', '08'), 
-			cover: 'img/book1.jpg'
+			cover: 'img/book1.jpg',
+			likes: 0,
+			dislikes: 0
 		}, 
 		{
 			name: 'The song of ice and fire',
 			price: 80,
 			pubdate: new Date('2009', '03', '08'), 
-			cover: 'img/book2.jpg'
+			cover: 'img/book2.jpg',
+			likes: 0,
+			dislikes: 0
+		}
+	];
+
+	$scope.plusOne = function(index) {
+		$scope.products[index].likes += 1; 
+	};
+
+	$scope.minusOne = function(index) {
+		$scope.products[index].dislikes += 1;
+	};
+	
+	//for mobile app market
+	$scope.move = {
+		icon: 'img/move.jpg',
+		title: 'MOVE',
+		developer: 'MOVE, Inc.',
+		price: 0.99
+	};
+
+	$scope.shutterbugg = {
+		icon: 'img/shutterbugg.jpg',
+		title: 'Shutterbugg',
+		developer: 'Chico Dusty',
+		price: 2.99
+	};
+
+	$scope.gameboard = {
+		icon: 'img/gameboard.jpg',
+		title: 'Gameboard',
+		developer: 'Armando P.',
+		price: 1.99
+	};
+
+	$scope.apps = [
+		{
+			icon: 'img/move.jpg',
+			title: 'MOVE',
+			developer: 'MOVE, Inc.',
+			price: 0.99
+		},
+		{
+			icon: 'img/shutterbugg.jpg',
+			title: 'MOVE',
+			developer: 'MOVE, Inc.',
+			price: 0.99
+		},
+		{
+			icon: 'img/gameboard.jpg',
+			title: 'MOVE',
+			developer: 'MOVE, Inc.',
+			price: 0.99
+		},
+		{
+			icon: 'img/move.jpg',
+			title: 'MOVE',
+			developer: 'MOVE, Inc.',
+			price: 0.99
 		}
 	];
 }]);
